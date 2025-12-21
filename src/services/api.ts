0d038958bf14c5df7da1890ea37c08a2384e8836
Léon Muselli <leon.muselli@etu.emse.fr>
2025-12-21 22:21:32 +0100

    add docker conf

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import { getToken, updateToken } from "./keycloak";
 
 const apiClient = axios.create({
-    baseURL: "http://localhost:8080/api",
+    baseURL: "/api",
     headers: {
         "Content-Type": "application/json",
     },
